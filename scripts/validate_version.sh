ef779ad2d94c0f723131cd473cd7d2745d29713d
Charles Korn <me@charleskorn.com>
2020-10-26 09:40:58 +1100

    Add script to validate that Git tag matches version in files on release.

---
+++ scripts/validate_version.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+
+function main() {
+    VERSION_IN_SCRIPT=$(getVersionInScript)
+    GIT_TAG=$(getGitTag)
+
+    echo "Version in script: $VERSION_IN_SCRIPT"
+    echo "Git tag: $GIT_TAG"
+
+    if [ "$VERSION_IN_SCRIPT" == "$GIT_TAG" ]; then
+        echo "Everything looks good."
+        exit 0
+    else
+        echo "Version in file does not match tag!"
+        exit 1
+    fi
+}
+
+function getVersionInScript() {
+    sed -E -n 's/set -lx BATECT_COMPLETION_PROXY_VERSION \"(.*)\"/\1/p' "$ROOT_DIR/completions/batect.fish" | tr -d " "
+}
+
+function getGitTag() {
+    git describe --dirty --candidates=0
+}
+
+main
