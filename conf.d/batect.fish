26c45a298d0b93166f9a829334dee992269367da
Charles Korn <me@charleskorn.com>
2023-05-14 12:04:16 +1000

    Simplify setup with Fisher.

---
+++ conf.d/batect.fish
@@ -0,0 +1,3 @@
+# This is needed because of the fix in https://github.com/fish-shell/fish-shell/issues/9133, which
+# breaks completions for commands not on PATH.
+source $__fish_config_dir/completions/batect.fish
